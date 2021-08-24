90b178d3314e5c821eac1157f3654ccd0b22bf5b
Fola_Ode <afolaode@gmail.com>
2021-08-24 17:00:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HomePage from './homepage';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    )
+  }
+}
+
+export default App
