f4c0c50b97fe31346d0eb061a0e75de678a0e266
Fola_Ode <afolaode@gmail.com>
2021-08-26 17:58:28 +0100

    sign-in form and button added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
+
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import Header from "./components/header/header";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-out";
 
 import "./App.css";
 
@@ -13,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path= "/" component = {HomePage} />
           <Route path= "/shop" component = {ShopPage} />
+          <Route path ="/signin" component ={SignInAndSignUpPage} />
         </Switch>
       </div>
     )
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-out.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in';
+
+import "./sign-in-and-sign-out.scss"
+
+const signInAndSignOutPage = () => {
+    return (
+        <div className ="sign-in-and-sign-up">
+            <SignIn />
+        </div>
+    )
+}
+
+export default signInAndSignOutPage;
